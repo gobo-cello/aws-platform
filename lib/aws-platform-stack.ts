d29b3cdec0661c3676c0705ea182227b24b44dcb
Tomoya Murata <171334196+gobo-cello@users.noreply.github.com>
2026-07-19 16:48:46 +0900

    chore(biome): PR CI Gate の整備 (#3)

--- lib/aws-platform-stack.ts
+++ lib/aws-platform-stack.ts
@@ -1,16 +1,4 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib/core";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AwsPlatformStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsPlatformQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
+export class AwsPlatformStack extends cdk.Stack {}
